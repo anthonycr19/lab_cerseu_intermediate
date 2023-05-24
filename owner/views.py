ff713134664ddfc23f2fbc5d9be17703673b5467
“Anthony <acarrillorj@gmail.com>
2023-05-23 22:16:58 -0500

    add teplates and use views

--- owner/views.py
+++ owner/views.py
@@ -3,11 +3,65 @@ from django.shortcuts import render
 
 # Create your views here.
 def owner_list(request):
-    data_context = {
-        'nombre': 'Katty Paredes',
-        'edad': 24,
-        'dni': 88842232,
-        'pais': 'Perú'
-    }
+    # data_context = {
+    #     'nombre': 'Katty Paredes',
+    #     'edad': 16,
+    #     'dni': 88842232,
+    #     'pais': 'Perú',
+    #     'vigente': False
+    # }
 
-    return render(request, 'owner/owner_list.html', data_context)
+    data_context = [
+        {
+            'nombre': 'Katty Paredes',
+            'edad': 16,
+            'dni': 88842232,
+            'pais': 'Perú',
+            'vigente': False,
+            'pokemons': [
+                {
+                    'nombre_pokemon': 'Charizard',
+                    'ataques': ['Ataque 1 - Charizard', 'Ataque 2 - Charizard', 'Ataque 3 - Charizard']
+                }
+            ]
+        },
+        {
+            'nombre': 'Miguel Valera',
+            'edad': 26,
+            'dni': 43452345,
+            'pais': 'España',
+            'vigente': False,
+            'pokemons': [
+                {
+
+                }
+            ]
+        },
+        {
+            'nombre': 'Jesús de la Cruz',
+            'edad': 30,
+            'dni': 88842232,
+            'pais': 'Colombia',
+            'vigente': False,
+            'pokemons': [
+                {
+
+                }
+            ]
+        },
+        {
+            'nombre': 'Liliana Vargas',
+            'edad': 24,
+            'dni': 85552232,
+            'pais': 'España',
+            'vigente': True,
+            'pokemons': [
+                {
+
+                }
+            ]
+        }
+    ]
+
+    return render(request, 'owner/owner_list.html', context={'data': data_context})
+
